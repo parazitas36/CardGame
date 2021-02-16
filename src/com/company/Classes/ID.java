0af636d55fc5ba4e016bf17dd10a72c4d20bdca6
parazitas36 <lukaslg96@gmail.com>
2021-02-16 12:02:32 +0200

    Added GameObject class

--- src/com/company/Classes/ID.java
+++ src/com/company/Classes/ID.java
@@ -5,5 +5,15 @@ public enum ID {
     Curse,
     Monster,
     Player1,
-    Player2
+    Player2,
+    Player1_Slot1,
+    Player1_Slot2,
+    Player1_Slot3,
+    Player1_Slot4,
+    Player1_Slot5,
+    Player2_Slot1,
+    Player2_Slot2,
+    Player2_Slot3,
+    Player2_Slot4,
+    Player2_Slot5
 }
