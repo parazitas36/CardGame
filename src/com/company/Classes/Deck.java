ed123d5427fda96ace5ee09b3f76fca5f916748d
Smygolz <martynasravinis@gmail.com>
2021-02-18 14:06:05 +0200

    Dekas

--- src/com/company/Classes/Deck.java
+++ src/com/company/Classes/Deck.java
@@ -1,11 +1,15 @@
 package com.company.Classes;
 
+import com.company.Utils.CardReader;
+
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 import java.util.Stack;
 
 public class Deck extends GameObject{
-    private Stack deck1;
+     ArrayList <Card> cards;
     Stack<Card> deck;
     private int size;
     public Deck(int _size, int x, int y) {
@@ -13,10 +17,41 @@ public class Deck extends GameObject{
         this.deck = new Stack<Card>();
         this.size = _size;
     }
+    public Deck(int _size, int x, int y, ArrayList<Card> _card) {
+        super();
+        this.size = _size;
+        this.deck = new Stack<Card>();
+        this.transfer(_card);
+    }
+
+    public void transfer(ArrayList<Card> card) {
+        for (int i=0 ; i < card.size(); i++) {
+            deck.add(card.get(i));
+        }
+    }
 
     @Override
     public void tick() {
 
+    }
+    public void add(Card card) { deck.add(card); }
+
+     public void shuffle() {
+        ArrayList<Card> temp = new ArrayList<>();
+        while(!deck.isEmpty()) {
+            temp.add(deck.pop());
+        }
+
+        Collections.shuffle(temp);
+        for (int i = 0; i < temp.size(); i++) {
+            deck.push(temp.get(i));
+        }
+     }
+
+    public Card drawRandomCard() {
+        Random generator = new Random();
+        int index = generator.nextInt(deck.size());
+        return deck.remove(index);
     }
 
     @Override
--- src/com/company/Utils/CardReader.java
+++ src/com/company/Utils/CardReader.java
@@ -1,24 +1,25 @@
 package com.company.Utils;
 
-import com.company.Classes.Buff;
-import com.company.Classes.Card;
-import com.company.Classes.Curse;
-import com.company.Classes.Monster;
+import com.company.Classes.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class CardReader {
 
-    public static void Read(String path) {
-        ArrayList<Card> cards = new ArrayList<Card>();
+
+    public static ArrayList<Card> Read(String path) {
+        ArrayList<Card> cards = new ArrayList<>();
         Scanner scan = null;
         try {
+
             scan = new Scanner(new File(path));
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -28,35 +29,35 @@ public class CardReader {
             String type = values[0];
             String name = values[1];
             int manaCost = Integer.getInteger(values[2]);
+            int x = Integer.getInteger(values[3]);
+            int y = Integer.getInteger(values[4]);
             switch (type) {
                 case "M" :
-                    Integer attack = Integer.getInteger(values[3]);
-                    Integer defence = Integer.getInteger(values[4]);
+                    int attack = Integer.getInteger(values[5]);
+                    int defence = Integer.getInteger(values[6]);
 
-                   // Monster monster = new Monster(name, manaCost, attack, defence);
-
-
+                //    Monster monster = new Monster(name, manaCost, x, y, attack, defence);
 
                 case "B":
-                    Integer _attack = Integer.getInteger(values[3]);
-                    Integer _defence = Integer.getInteger(values[4]);
-                    Integer hp = Integer.getInteger(values[5]);
+                    int _attack = Integer.getInteger(values[3]);
+                    int _defence = Integer.getInteger(values[4]);
+                    int hp = Integer.getInteger(values[5]);
 
-                   // Buff buff = new Buff(name, manaCost, _attack, _defence, hp);
+                     // Buff buff = new Buff(name, manaCost, _attack, _defence, hp);
 
                 case "C":
-                    Integer __attack = Integer.getInteger(values[3]);
-                    Integer __defence = Integer.getInteger(values[4]);
-                    Integer __stun = Integer.getInteger(values[5]);
+                    int __attack = Integer.getInteger(values[3]);
+                    int __defence = Integer.getInteger(values[4]);
+                    int __stun = Integer.getInteger(values[5]);
 
                    // Curse curse = new Curse(name, manaCost, __attack, __defence, __stun);
-
-
             }
-
-
         }
+        Collections.shuffle(cards);
+        return cards;
     }
+
+
 
 }
 
